427ce816d3529be29e16b9c69c6eef52f9fb5bb5
saisuresh <saisureshv@adityauniversity.in>
2026-03-24 16:08:35 +0530

    yes

---
+++ index2.js
@@ -0,0 +1,7 @@
+const form = document.querySelector(".feedback-form");
+form.addEventListener("submit",(event)=>{
+    event.preventDefault();
+   console.log(form.username.value);
+   console.log(form.feedback.value);
+
+})
