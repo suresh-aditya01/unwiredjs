f9b7837ce7b94599f8c496f0e7c97ffc18875742
saisuresh <saisureshv@adityauniversity.in>
2026-03-25 16:29:56 +0530

    yes

--- index2.js
+++ index2.js
@@ -1,7 +1,37 @@
 const form = document.querySelector(".feedback-form");
+const p = document.querySelector("p");
+const usernamePattern=/^[a-zA-Z0-9]{6,10}$/;
+const feedbackPattern =/^.{20,50}$/;
 form.addEventListener("submit",(event)=>{
     event.preventDefault();
-   console.log(form.username.value);
-   console.log(form.feedback.value);
+    const userResult =usernamePattern.test(form.username.value);
+    const feedabackResult = feedbackPattern.test(form.feedback.value);
 
-})
+if(userResult && feedabackResult){
+p.setAttribute("class","success");
+p.textContent="Feedback submitted, Thank you !";
+}else{
+p.setAttribute("class","error");
+p.textContent="please check username and feedback again!";
+}
+});
+form.username.addEventListener("keyup",(event)=>{
+    const userResult =usernamePattern.test(event.target.value);
+    if(userResult){
+        form.username.setAttribute("class","accepted");
+    }
+    else{
+        form.username.setAttribute("class","rejected");
+    }
+});
+form.feedback.addEventListener("keyup",(event)=>{
+    const feedbackResult =feedbackPattern.test(event.target.value);
+    if(feedbackResult){
+        form.feedback.setAttribute("class","accepted");
+    }
+    else{
+        form.feedback.setAttribute("class","rejected");
+    }
+});
+
+
